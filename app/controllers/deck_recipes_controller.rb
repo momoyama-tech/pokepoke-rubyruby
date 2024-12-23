d99add6d56ab2b4ac056464d0afb30d75fbd7a04
Hideyasu <kutuzov1228@gmail.com>
2024-12-23 11:23:18 +0000

    feat: bootstrapのお手本を追加

--- app/controllers/deck_recipes_controller.rb
+++ app/controllers/deck_recipes_controller.rb
@@ -1,2 +1,5 @@
 class DeckRecipesController < ApplicationController
+  def index
+    @deck_recipes = DeckRecipe.all
+  end
 end
--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,2 +1,5 @@
 class RatesController < ApplicationController
+  def index
+    @rates = Rate.all
+  end
 end
