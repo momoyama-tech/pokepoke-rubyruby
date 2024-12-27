9e7eec0b476ec5cc6b10df75a935748ee0e2486c
Hideyasu <kutuzov1228@gmail.com>
2024-12-27 11:13:33 +0900

    feat: レシピのカード一覧を表示

--- app/models/deck_recipe.rb
+++ app/models/deck_recipe.rb
@@ -1,2 +1,3 @@
 class DeckRecipe < ApplicationRecord
+  has_many :deck_recipes_poke_cards
 end
--- app/models/deck_recipes_poke_card.rb
+++ app/models/deck_recipes_poke_card.rb
@@ -1,2 +1,4 @@
 class DeckRecipesPokeCard < ApplicationRecord
+  belongs_to :deck_recipe
+  belongs_to :poke_card
 end
