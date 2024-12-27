400680de7331efca8e234e580ffb477d127198dc
satsuki2200 <132316826+satsuki2200@users.noreply.github.com>
2024-12-27 03:31:22 +0000

    chore:migrate実行しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_26_153244) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_27_030246) do
   create_table "deck_recipes", force: :cascade do |t|
     t.integer "user_id"
     t.string "name", null: false
     t.string "description"
-    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_deck_recipes_on_user_id"
